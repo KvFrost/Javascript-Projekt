6c561a802f42ab5a1ab2eb478465b1cd3b027147
KvFrost <kristine.frost@hotmail.dk>
2024-02-14 11:34:38 +0100

    Opdatering

--- search.js
+++ search.js
@@ -1,8 +1,6 @@
-// Array med søgeord
 var searchTerms = ['sko', 'jenas', 'jakker','tøj', 'beauty', 'sport','accessories'];
 
 
-// Funktion til at skifte søgefeltets tilstand
 function toggleSearch() {
     var searchBar = document.getElementById('searchBar');
     if (searchBar.classList.contains('active')) {
@@ -12,16 +10,13 @@ function toggleSearch() {
     }
 }
 
-// Funktion til at udføre søgning
 function search() {
-    // Gennemgå hvert søgeord i arrayet
     for (var i = 0; i < searchTerms.length; i++) {
         var searchTerm = searchTerms[i];
         if (searchTerm !== '') {
             console.log('Søger efter: ' + searchTerm);
-            // Implementer søgelogikken her
         } else {
-            console.log('Indtast venligst et søgeord for felt ' + (i + 1));
+            console.log('Indtast venligst et søgeord ' + (i + 1));
         }
     }
   }
