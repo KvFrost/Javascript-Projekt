f7f09dd87835f0846b3cc91000c2c143c8b8168f
Freyaui <freya.sostack@gmail.com>
2024-02-15 12:25:38 +0100

    gallery

--- slideshow.js
+++ slideshow.js
@@ -1,19 +1,27 @@
-const button = document.querySelectorAll("[data-carousel-button]")
+let slideIndex = 1;
+showSlides(slideIndex);
 
-button.forEach(button => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]")
+function plusSlides(x) {
+  showSlides(slideIndex += x);
+}
 
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+function currentSlide(x) {
+  showSlides(slideIndex = x);
+}
 
-    if (newIndex < 0) newIndex = slides.children.length -1
-    if (newIndex >= slides.children.length) newIndex = 0
+function showSlides(x) {
+  let t;
+  let slides = document.getElementsByClassName("slideshoow");
+  let dots = document.getElementsByClassName("dots");
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-  })
-})
+  if (x > slides.length) { slideIndex = 1 };
+  if (x < 1) { slideIndex = slides.length };
+  for (t = 0; t < slides.length; t++) {
+    slides[t].style.display = "none";
+  }
+  for (t = 0; t < dots.length; t++) {
+    dots[t].className = dots[t].className.replace("active", "");
+  }
+  slides[slideIndex -1].style.display = "block";
+  dots[slideIndex -1].className += "active";
+}
