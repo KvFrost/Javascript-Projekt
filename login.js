36e4466ddfc4f4a6d463ba670e9960852bf40659
chamelzer <charlottemelzer@hotmail.com>
2024-02-15 11:05:08 +0100

    login kode

---
+++ login.js
@@ -0,0 +1,15 @@
+document
+  .getElementById("login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "password") {
+      alert("Login successful");
+    } else {
+      document.getElementById("error-message").textContent = alert(
+        "E-mail eller password er desværre forkert. Prøv igen."
+      );
+    }
+  });
